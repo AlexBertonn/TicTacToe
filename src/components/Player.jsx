c3952ba9bbcce1c9773ceda872ffaa8cd1ac276e
Alex Berton <alexberton47@gmail.com>
2024-11-12 20:15:32 -0300

    Finishing the choice of player names
    In this commit I finished the step of changing names in inputs.
    Create a useState and a function.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
-    function handleSelectButton() {
-        setIsEditing(true)
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
     };
 
-    let playerName = <span className="player-name">{name}</span>
+
+    function handleSelectButton() {
+        setIsEditing((editing) => !editing)
+    };
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
     if(isEditing){
-        playerName = <input type="text" required/>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     }
 
     return (
             <li>
                 <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
                 </span>
-                <button onClick={handleSelectButton}>Edit</button>
+                <button onClick={handleSelectButton}>{isEditing ? "Save" : "Edit"}</button>
             </li>
 )}
