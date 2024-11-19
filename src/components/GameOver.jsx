749dc3c78d24d0ba7b24c4b7195956df56f2b020
Alex Berton <104038811+AlexBertonn@users.noreply.github.com>
2024-11-19 10:13:44 +0000

    .

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 export default function GameOver({winner, onRestart}) {
     return(
         <div id='game-over'> 
-            <h2>Game Over!</h2>
+            <h2>Game Over</h2>
             {winner && <p> {winner} Won!</p>}
             {!winner && <p>It's Draw!</p>}
             <p>
